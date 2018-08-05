cfd9f64a53b89f738a2fb306c8cdb7ebe1f20ddc
hoantran <hoan@bluepego.com>
2018-08-05 16:36:01 -0700

    Set up navigation bar

--- jpmc/AppDelegate.m
+++ jpmc/AppDelegate.m
@@ -22,32 +22,27 @@
     
 //    self.window.rootViewController = [[ViewController alloc] init];
 //    self.window.backgroundColor = [UIColor cyanColor];
+    
+    
+//    UINavigationBar.appearance().barTintColor = UIColor.white
+//    let controller = UINavigationController()
+//    let img = UIImage()
+//    controller.navigationBar.shadowImage = img
+//    window = UIWindow(frame: UIScreen.main.bounds)
+//    window?.makeKeyAndVisible()
+//    window?.rootViewController = controller
+    
+//    [[UINavigationBar appearance] setTintColor:[UIColor redColor]];
 
-    UITabBarController *tabs = [[UITabBarController alloc] init];
-    tabs.viewControllers = [self initializeTabBarItems];
-    self.window.rootViewController = tabs;
+//    window?.rootViewController = UINavigationController(rootViewController: MainController())
+    
+    ViewController *controller = [[ViewController alloc] init];
+    self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:controller];
+    self.window.backgroundColor = [UIColor whiteColor];
     
     [self.window makeKeyAndVisible];
     return YES;
 }
-
-- (NSArray*) initializeTabBarItems {
-    UIViewController *vc1 = [[UIViewController alloc] init];
-    vc1.view.backgroundColor = [UIColor blueColor];
-    vc1.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemSearch tag:0];
-    UINavigationController *nav1 = [[UINavigationController alloc] initWithRootViewController:vc1];
-    nav1.title = @"BLUE";
-
-    UIViewController *vc2 = [[UIViewController alloc] init];
-    vc2.view.backgroundColor = [UIColor redColor];
-    vc2.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemHistory tag:1];
-    UINavigationController *nav2 = [[UINavigationController alloc] initWithRootViewController:vc2];
-    nav2.title = @"RED";
-
-    NSArray *vcArray = [NSArray arrayWithObjects:vc1,vc2, nil];
-    return vcArray;
-}
-
 
 - (void)applicationWillResignActive:(UIApplication *)application {
 }
--- jpmc/controllers/ViewController.m
+++ jpmc/controllers/ViewController.m
@@ -22,14 +22,65 @@ RestService *svr;
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    svr = [[RestService alloc] init];
-    [svr get:@"https://api.spacexdata.com/v2/launches/all" success:^(NSData * _Nonnull data) {
-        NSError* err = nil;
-        self.launches = [LaunchModel arrayOfModelsFromData:data error:&err];
-        NSLog(@"ok");
-    }];
+    
+//    svr = [[RestService alloc] init];
+//    [svr get:@"https://api.spacexdata.com/v2/launches/all" success:^(NSData * _Nonnull data) {
+//        NSError* err = nil;
+//        self.launches = [LaunchModel arrayOfModelsFromData:data error:&err];
+//        NSLog(@"ok");
+//    }];
+    
+//    UINavigationBar* navbar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+//
+//    UINavigationItem* navItem = [[UINavigationItem alloc] initWithTitle:@"karthik"];
+//    // [navbar setBarTintColor:[UIColor lightGrayColor]];
+//    UIBarButtonItem* cancelBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(onTapCancel:)];
+//    navItem.leftBarButtonItem = cancelBtn;
+//
+//    UIBarButtonItem* doneBtn = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(onTapDone:)];
+//    navItem.rightBarButtonItem = doneBtn;
+//
+//    [navbar setItems:@[navItem]];
+//    [self.view addSubview:navbar];
+    
+    
+    self.navigationItem.title = @"All Launches";
+    
+//    UIButton *imageButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 35, 35)];
+//    [imageButton setBackgroundImage:[UIImage imageNamed:@"filter.png"] forState:UIControlStateNormal];
+//    [imageButton addTarget:self action:@selector(messageButtonTapped) forControlEvents:UIControlEventAllEvents];
+//    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithCustomView:imageButton];
+//    self.navigationItem.rightBarButtonItem = item;
+    
+//    var imageLogo = UIImage(named: "LogoWithTextSmaller.png")
+//    imageLogo = imageLogo?.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
+//    btnLogo.setImage(imageLogo, for: .normal)
+    
+    
+    [self setupFilterButton];
 }
 
+- (void) setupFilterButton {
+    UIImage *image = [UIImage imageNamed: @"filter.png"];
+    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    
+    UIBarButtonItem *_btnLeftBar = [[UIBarButtonItem alloc]initWithImage:image
+                                                                   style:UIBarButtonItemStylePlain
+                                                                  target:self
+                                                                  action:@selector(handleFilterTapped)];
+    float itemWidth = self.navigationItem.rightBarButtonItem.width;
+    float leftOffset = image.size.width - itemWidth;
+    _btnLeftBar.imageInsets = UIEdgeInsetsMake(0, leftOffset, 0, 0);
+    
+    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+    spacer.width = 0;
+    self.navigationItem.rightBarButtonItems = @[spacer,_btnLeftBar];
+}
+
+-(void)handleFilterTapped {
+    NSLog(@"Filter tapped");
+}
 
 @end
 
