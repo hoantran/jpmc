c46fbd92060148b0ba9b126ca719e2f5f45989cf
Hoan <hoantran@users.noreply.github.com>
2018-08-06 21:17:58 -0700

    Update AppDelegate.m

--- jpmc/AppDelegate.m
+++ jpmc/AppDelegate.m
@@ -20,6 +20,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
+    // ..............................................................................................
+    // For a simple app such as this one that has a small number of screens,
+    // this common practice of allowing one viewController calling another directly is sufficient and effective.
+    // For a larger app where switching graph between the many controllers are more complex,
+    // a coordinator pattern or the likes would be more appropriate to move the routing responsibilities 
+    // from the viewControllers onto objects specifically designed for this role.
+    // These patterns separate the routing concerns from the viewControllers leaving them
+    // more concentrated on UI tasks.
+    // ..............................................................................................
+    
     LaunchController *controller = [[LaunchController alloc] init];
     self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:controller];
     self.window.backgroundColor = [UIColor whiteColor];
